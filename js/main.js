95bbd78da0f018f5a949bed56c43823ea5d4444c
slocodemonkey <stillmattwest@gmail.com>
2013-09-26 15:38:53 -0700

    user phrase working
    validation working, app breaks user phrase into an array as required

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
 $(document).ready(function(){
 
-	$('#submit').click(function(){
-		
-		
+	var userPhrase ='';
 	
-
+	var updatePhrase = function(phrase){
+		$('#result_text').html('<p>'+phrase+'</p>');
+		console.log(phraseArray);
+	};
+	
+	var phraseArray = [];
+	
+	var makeArray = function(phrase){
+		
+		for(i=0;i<phrase.length;i++){
+			phraseArray.push(phrase.charAt(i));
+			
+		}
+		updatePhrase(phrase);
+	};
+		
+	$('#phrase_form').submit(function(){
+		
+		userPhrase = document.getElementById("user_entry").value
+		
+		makeArray(userPhrase);
+		return false; // prevents submit from reloading page and reverting phrase text
+	
 	}); // ends submit.click
+	
+	
 
 }); // ends document.ready
