fb09b51efdd2c42167cdbe8b0cc95180b52e1eb9
slocodemonkey <stillmattwest@gmail.com>
2013-10-02 16:03:11 -0700

    appears to work
    program evolves words, which is awesome.  No recognition of this as of
    yet.  Only rudimentary presentation.

--- js/main.js
+++ js/main.js
@@ -25,8 +25,21 @@ var evolution = {
 	},
 	
 	display: function(array){
-		for(var i=0;i<array.length;i++)
+		$('#results').html('<div><h2>The User Phrase is: '+this.userPhrase+'</h2></div>')
+		
+		for(var i=0;i<array.length;i++){
+				
 			$('#results').append('<div><p>'+array[i].name+':  '+array[i].value.join('')+', '+array[i].score+'</p></div>')
+			}
+			
+		/* if(array[i].value.join('') == this.userPhrase){
+			$('$results').append('<div><h2>MATCH!</h2></div>')
+			return false;
+				
+		} */
+		
+		
+			
 	}, // ends display method
 	
 	scoreIt: function(array,func){
@@ -35,11 +48,11 @@ var evolution = {
 	}, // ends scoreIt method note
 	
 	lengthScore: function(array){
-		var lengthScore = 100 - (this.userPhrase.length - array.length);
-		if(lengthScore < 0){
-			lengthScore *= -1;
-		}
-		
+		var difference = ((this.userPhrase.length -1) - (array.length -1));
+		if(difference < 0){
+			difference *= -1;
+		}	
+		var lengthScore = 100 - (5*difference);
 		return lengthScore;
 	},
 	
@@ -52,7 +65,7 @@ var evolution = {
 			if(temp<0){
 				temp *= -1;
 			}
-			temp = (27-temp);
+			temp = (27-(2*temp));
 			charScore += temp;
 		}
 		
@@ -66,7 +79,7 @@ var evolution = {
 			var seedling = [];
 			var length = this.getRandom(1,100);
 			for(var j=0;j<length;j++){  //creates seedling
-				var random = this.getRandom(1,this.alphaArray.length);
+				var random = this.getRandom(0,this.alphaArray.length);
 				seedling.push(this.alphaArray.charAt(random));
 			} // end seedling creation loop
 			var seedName = 'gen'+this.generation+'seed'+i;
@@ -81,6 +94,9 @@ var evolution = {
 			return 0;
 		}); // sorts the seed array by score property
 		
+		console.log(this.alphaArray[0]);
+		console.log(this.alphaArray.indexOf('a'));
+		
 		this.display(this.seedArray);
 		this.matchMaker(this.seedArray);
 		this.seedArray=[]; // clears seedArray for next generation
@@ -88,6 +104,8 @@ var evolution = {
 	},
 	matchMaker:function(array){ // selects four best phrases and splits them at random into two couples and sends couples to breeder
 		array.splice(4);
+		this.coupleOne=[]; //clears C1 for next generation
+		this.coupleTwo=[]; //clears C2 for next generation
 		this.generation++;
 		while(array.length > 0){
 			var chooser = this.getRandom(0,(array.length-1));
@@ -99,22 +117,19 @@ var evolution = {
 				array.splice(chooser,1);
 			}
 		}
-		console.log(this.coupleOne);
-		console.log(this.coupleTwo);
 		this.breeder(this.coupleOne);
 		this.breeder(this.coupleTwo);
+		
 
 	}, // ends matchMaker function
 	
 	breeder:function(couple){ // creates 'child' array based on value of couple passed by matchMaker function
-		console.log(couple[0].value[0]);
 		for(var i = 0;i<6;i++){ // set initial variables, six children per parent couple.
-			console.log('I is in ur loop');
 			var child = [];
 			var stop = false;
 			var j = 0;
 			while(stop == false){
-			
+				
 			//determines which character should be pushed to child array. Options are the corresponding value from parent 1, parent 2, a range of values between parent 1 and 2, or a random mutation
 			
 			//if a parent value check comes up false, value is inherited from the longer parent value.  Else determined by inheritance rules below
@@ -132,34 +147,37 @@ var evolution = {
 				}
 				
 			// set inheritance rules
+				
+				
 				var inheritance = this.getRandom(1,100);
-				if(inheritance < 33){
+				if(inheritance < 11){
 					child.push(first);
-				}else if(inheritance < 65){
+				}else if(inheritance < 21){
 					child.push(second);
-				}else if(inheritance < 97){
-					var range = this.getRandom(first,second);
-					child.push(range);
+				}else if(inheritance < 95){
+					var range = this.getRandom(this.alphaArray.indexOf(first),this.alphaArray.indexOf(second));
+					child.push(this.alphaArray.charAt(range));
+					console.log('first '+first+'second '+second);
 				}else{
-					var mutation = this.getRandom(1,this.alphaArray.length);
+					var mutation = this.getRandom(0,this.alphaArray.length);
 					child.push(this.alphaArray.charAt(mutation));
 				}
 				
 				// once inheritance has been carried out for all elements of value array from both parents, determines length of child array.  Similar to how character values are inherited.
 				if(!couple[0].value[j] && !couple[1].value[j]){
 					var inheritLength = this.getRandom(1,100)
-					if(inheritLength < 33){
+					if(inheritLength < 11){
 						child.splice(couple[0].value.length);
-					}else if(inheritLength < 65){
+					}else if(inheritLength < 21){
 						child.splice(couple[1].value.length);
-					}else if(inheritLength <97){
-						var lengthRange = this.getRandom(couple[0].value.length,couple[1].value.length);
-						child.splice(lengthRange);
+					}else if(inheritLength < 95){
+						var childLength = this.getRandom(couple[0].value.length,couple[1].value.length);
+						child.splice(childLength);
 					}else{
-						var lengthMutation = this.getRandom(-10,10);
+						var lengthMutation = this.getRandom(-5,5);
 						if(lengthMutation>0){
 							for(var k=0;k<lengthMutation;k++){
-								child.push(this.alphaArray.charAt(this.getRandom(1,this.alphaArray.length)));
+								child.push(this.alphaArray.charAt(this.getRandom(0,this.alphaArray.length)));
 							}
 						}else{
 							child.splice(child.length + lengthMutation);
@@ -168,12 +186,13 @@ var evolution = {
 				
 					stop = true;
 				} 
-				console.log(child.join());
+				
 				j++;
 			} // ends while loop
-			if(this.seedArray.length > 6){ // looking for existing children in seedArray
+			
+			if(this.seedArray.length > 5){ // looking for existing children in seedArray
 				var childName = 'gen'+this.generation+'seed'+(6+i);
-				this.seedArray[6+i] = new this.Seed(childName,child);
+				this.seedArray[(6+i)] = new this.Seed(childName,child);
 			}else{
 			var childName = 'gen'+this.generation+'seed'+i;
 			this.seedArray[i] = new this.Seed(childName,child);
@@ -192,7 +211,7 @@ var evolution = {
 				return 0;
 				}); // sorts the seed array by score property
 			this.display(this.seedArray);
-			this.matchMaker(this.SeedArray);
+			this.matchMaker(this.seedArray);
 			this.seedArray=[]; // clears seedArray for next generation
 		}
 	}, // ends breeder function
